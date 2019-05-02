b1ae589b129e68fa5bfccb0f1652102036ab454c
Samuel <samy.david@clevertech.biz>
2019-05-02 08:54:06 -0400

    Added helmet for better security

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@
 
 // Express App Setup
 const express = require('express');
+const helmet = require('helmet')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const productsRepo = require('./persistence/pgProduct')
 const products = require('./routes/products')(new productsRepo())
 const app = express();
+app.use(helmet())
 app.use(cors());
 app.use(bodyParser.json());
 
